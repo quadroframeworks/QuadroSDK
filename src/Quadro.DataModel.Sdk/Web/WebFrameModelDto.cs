b715e3b86dc7f7d69c93e09f9caa474c955f3505
Mark Konst <markkonst@quadroframeworks.com>
2025-02-06 17:26:27 +0100

    Web updates

--- src/Quadro.DataModel.Sdk/Web/WebFrameModelDto.cs
+++ src/Quadro.DataModel.Sdk/Web/WebFrameModelDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Quadro.DataModel.Entities.Web
+﻿using Quadro.DataModel.Model;
+
+namespace Quadro.DataModel.Entities.Web
 {
 	public class WebFrameModelDto
 	{
+		public string? Description { get; set; }
+		public List<FrameMessageDto> Messages { get; set; } = new List<FrameMessageDto>();
 		public double PriceExVat { get; set; }
 		public string Svg { get; set; } = null!;
 	}
