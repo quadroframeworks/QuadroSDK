d8de2d6a4959b35d2926ab256a0ae3c9892ea707
Mark Konst <markkonst@quadroframeworks.com>
2025-02-08 16:02:21 +0100

    Working on it

---
+++ src/Quadro.Documents.Sdk/UnitOfWork/DataContainer.cs
@@ -0,0 +1,20 @@
+﻿using Quadro.Utils.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadro.Documents.UnitOfWork
+{
+
+    public class DataContainer
+    {
+        public string Id { get; set; } = null!;
+        public string Hash { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public IStorable Model { get; set; } = null!;
+        public DataDocument? ViewModel { get; set; }
+    }
+
+}
