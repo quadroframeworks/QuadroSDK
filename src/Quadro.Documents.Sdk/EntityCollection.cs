bcd882180147a6cf95cac10f633ba23d60a72599
Mark Konst <markkonst@quadroframeworks.com>
2025-02-14 09:03:42 +0100

    Working on refactor

---
+++ src/Quadro.Documents.Sdk/EntityCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadro.Documents.Sdk
+{
+    public class EntityCollection
+    {
+        public List<EntityPropertyHeader> Headers { get; set; } = new List<EntityPropertyHeader>();
+        public List<EntitySummary> Entities { get; set; } = new List<EntitySummary>();
+    }
+
+
+    public class EntityPropertyHeader
+    {
+        public string PropertyName { get; set; } = null!;
+        public List<NamingTranslation> Translations { get; set; } = new List<NamingTranslation>();
+    }
+
+    public class EntitySummary
+    {
+        public string Id { get; set; } = null!;
+        public string? FilterString { get; set; }
+        public List<string?> Values { get; set; } = new List<string?>();
+    }
+}
