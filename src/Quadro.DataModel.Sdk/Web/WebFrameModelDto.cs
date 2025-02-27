31b881db5cbc902593dd1f6d00beb97483f9dd25
Mark Konst <markkonst@quadroframeworks.com>
2025-02-27 17:31:35 +0100

    Working on webshop

--- src/Quadro.DataModel.Sdk/Web/WebFrameModelDto.cs
+++ src/Quadro.DataModel.Sdk/Web/WebFrameModelDto.cs
@@ -1,14 +1,15 @@
 ﻿using Quadro.DataModel.Model;
-using Quadro.Utils.DataContext;
+using Quadro.Documents.UnitOfWork;
+
 
 namespace Quadro.DataModel.Entities.Web
 {
-	public class WebFrameModelDto
+    public class WebFrameModelDto
 	{
 		public string? Description { get; set; }
 		public List<FrameMessageDto> Messages { get; set; } = new List<FrameMessageDto>();
 		public double PriceExVat { get; set; }
 		public string Svg { get; set; } = null!;
-        public UnitOfWork.UnitOfWork UnitOfWork { get; set; } = null!;
+        public UnitOfWork UnitOfWork { get; set; } = null!;
 	}
 }
